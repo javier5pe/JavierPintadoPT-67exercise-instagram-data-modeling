9dc6336d8da6549a2124a9033155e748b9fcf701
javier5pe <72091566+javier5pe@users.noreply.github.com>
2024-06-28 19:23:46 +0000

    n

--- src/models.py
+++ src/models.py
@@ -13,26 +13,12 @@ class User(Base):
     user_name = Column(String(50), nullable=False)
     password = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship('Person')
-    posts = relationship('Post', back_populates='user')
-    comments = relationship('Comment', back_populates='user')
-    followers = relationship('Followers', 
-                             primaryjoin="User.id==Followers.user_from_id",
-                             back_populates="user_from")
-    following = relationship('Followers',
-                             primaryjoin="User.id==Followers.user_to_id",
-                             back_populates="user_to")
-
+   
 class Followers(Base):
     __tablename__ = 'followers'
     user_from_id = Column(Integer, ForeignKey("user.id"), primary_key=True, nullable=False)
     user_to_id = Column(Integer, ForeignKey("user.id"), primary_key=True, nullable=False)
-    user_from = relationship("User", foreign_keys=[user_from_id], back_populates="followers")
-    user_to = relationship("User", foreign_keys=[user_to_id], back_populates="following")
+    
 
 class Comment(Base):
     __tablename__ = 'comment'
@@ -47,9 +33,7 @@ class Post(Base):
     __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
-    user = relationship("User", back_populates="posts")
-    media = relationship("Media", back_populates="post")
-    comments = relationship("Comment", back_populates="post")
+  
 
 class Media(Base):
     __tablename__ = 'media'
